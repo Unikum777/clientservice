c4ae072e697aee4cc45b2e46d6c5ffbfb275d56c
Yershov <yershov.n@mail.ru>
2017-06-12 13:25:29 +0300

    First Commit

---
+++ ClientService/Controllers/ClientsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClientService.Models;
+
+namespace ClientService.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private ClientServiceEntities db = new ClientServiceEntities();
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            try
+            {
+                var client_list = db.Clitents.ToList();
+                client_list.Sort();
+                return View(client_list);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+            
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Client client = db.Clitents.Find(id);
+                if (client == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(client);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Surname,Name,MiddleName,Phone")] Client client)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Clitents.Add(client);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(client);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Client client = db.Clitents.Find(id);
+                if (client == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(client);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+
+        // POST: Clients/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Surname,Name,MiddleName,Phone")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            try
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Client client = db.Clitents.Find(id);
+                if (client == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(client);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client client = db.Clitents.Find(id);
+            db.Clitents.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ClientService/Models/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ClientService.Models
+{
+    public class Client : IComparable
+    {
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Client otherTemperature = obj as Client;
+            if (otherTemperature != null)
+                return this.Surname.CompareTo(otherTemperature.Surname);
+            else
+                throw new ArgumentException("Object is not a Client");
+        }
+        public int Id { get; set; }
+
+        [DisplayName("Фамилия")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [RegularExpression(@"^[\S]{1,60}$", ErrorMessage = "Длина поля до 60 симв.")]
+        public string Surname { get; set; }
+
+        [DisplayName("Имя")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [RegularExpression(@"^[\S]{1,60}$", ErrorMessage = "Длина поля до 60 симв.")]
+        public string Name { get; set; }
+
+        [DisplayName("Отчество")]
+        [RegularExpression(@"^[\S]{1,60}$", ErrorMessage = "Длина поля до 60 симв.")]
+        public string MiddleName { get; set; }
+
+        [DisplayName("Номер телефона")]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [RegularExpression(@"^[\d\+\(\)\-]{6,16}$", ErrorMessage = "Номер телефона может содержать цифры, а также знаки: ( ) + -")]
+        public string Phone { get; set; }
+    }
+}
